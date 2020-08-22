822b66806490bff1c301628c0350edf94591875f
mackeich86 <67783240+mackeich86@users.noreply.github.com>
2020-08-22 14:04:53 +0300

    Create homework_8.java
    задание 8 Цикличный вывод данных на экран

---
+++ homework_8.java
@@ -0,0 +1,9 @@
+/*
+Необходимо с помощью цикла вывести на экран числа от 1 до 50.
+*/
+
+public class homework_8 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 51; i++) System.out.println(i);
+    }
+}
